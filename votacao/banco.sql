024ad47161b0b156bde3add15670baa3cc72d357
jeffany alves <jeffanyalves@gmail.com>
2018-07-25 08:53:37 -0300

    primeiro commit

---
+++ votacao/banco.sql
@@ -0,0 +1,35 @@
+﻿CREATE TABLE Eleitor (
+	nome varchar(255) NOT NULL,
+	cpf varchar(11) NOT NULL,
+	senha varchar(30) NOT NULL,
+	PRIMARY KEY (cpf)
+);
+
+CREATE TABLE Eleicao (
+	id serial NOT NULL,
+	nome varchar(255) NOT NULL,
+	descricao TEXT NOT NULL,
+	candidato1 INTEGER NOT NULL,
+	candidato2 INTEGER NOT NULL,
+	periodo DATE NOT NULL,
+	PRIMARY KEY (id),
+	FOREIGN KEY (candidato1) REFERENCES Candidato(id),
+	FOREIGN KEY (candidato2) REFERENCES Candidato(id)
+);
+
+CREATE TABLE Candidato (
+	nome varchar(255) NOT NULL,
+	id serial NOT NULL,
+	descricao TEXT NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Votos (
+	cpf_eleitor varchar(11) NOT NULL,
+	id_eleicao integer NOT NULL,
+	voto BOOLEAN NOT NULL,
+	CONSTRAINT Votos_fk_eleitor
+	FOREIGN KEY (cpf_eleitor) REFERENCES Eleitor(cpf),
+	CONSTRAINT Votos_fk_eleicao
+	FOREIGN KEY (id_eleicao) REFERENCES Eleicao(id)
+);
--- votacao/views.py
+++ votacao/views.py
@@ -4,7 +4,7 @@ from votacao import app
 
 @app.before_request
 def before_request():
-   g.db = psycopg2.connect("dbname=votacao user=postgres password=sousa123 host=127.0.0.1")
+   g.db = psycopg2.connect("dbname=votacao user=postgres password=xuxu21")
 
 @app.teardown_request
 def teardown_request(exception):
@@ -27,7 +27,7 @@ def cadastro():
         cpf = request.form['cpf']
         senha = request.form['password']
         cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
-        cur.execute("INSERT INTO eleitor (nome, cpf, senha) VALUES (%s, %s, %s)", (nome, cpf, senha))
+        cur.execute("INSERT INTO eleitor (nome, cpf, senha) VALUES ('%s', '%s', '%s')" %(nome, cpf, senha))
         g.db.commit()
         return redirect(url_for('login'))
 
@@ -52,12 +52,61 @@ def login():
                 return redirect(url_for('home'))
             else:
                 pass
-
+        return redirect(url_for('login'))
 
 @app.route('/home')
 def home():
+    if request.method == 'POST':
+        return render_template('criarvotacao.html')
     return render_template('home.html')
 
 
+@app.route('/criarvotacao', methods=['GET', 'POST'])
+def criarvotacao():
+    cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute('SELECT * FROM candidato')
+    candidatos = cur.fetchall()
+    if (request.method == 'POST'):
+        nome = request.form['nome']
+        descricao = request.form['descricao']
+        candidato_um = request.form['candidato']
+        candidato_dois = request.form['candidatos']
+        periodo = request.form['periodo']
+        query = "INSERT INTO eleicao (nome, descricao, candidato1, candidato2, periodo) VALUES ('{}', '{}', {}, {}, '{}')".format(nome, descricao, candidato_um, candidato_dois, periodo)
+        cur.execute(query)
+        cur.execute("SELECT * FROM eleicao WHERE candidato1 = {} and candidato2 = {}".format(candidato_um, candidato_dois))
+        eleicao = cur.fetchone()
+        g.db.commit()
+        return redirect(url_for('sucesso', id_votacao = eleicao[0]))
+    return render_template('criarvotacao.html', candidatos = candidatos)
 
-            
+@app.route('/cadastrocandidato', methods=['GET', 'POST'])
+def cadastrocandidato():
+    if request.method == 'POST':
+        nome = request.form['nome']
+        descricao = request.form['descricao']
+        nome1 = request.form['nome1']
+        descricao1 = request.form['descricao1']
+        cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+        cur.execute("INSERT INTO candidato (nome, descricao) VALUES ('%s', '%s')" %(nome, descricao)) 
+        cur.execute("INSERT INTO candidato (nome, descricao) VALUES ('%s', '%s')" %(nome1, descricao1)) 
+        g.db.commit()
+        return redirect(url_for('criarvotacao'))
+    return render_template('cadastrocandidato.html')
+
+@app.route('/sucesso/<int:id_votacao>')
+def sucesso(id_votacao):
+    return render_template('sucesso.html', id_votacao = id_votacao)
+
+@app.route('/testar/<int:id_votacao>')
+def testar(id_votacao):
+    cur = g.db.cursor(cursor_factory=psycopg2.extras.DictCursor)
+    cur.execute("SELECT * FROM eleicao WHERE id = '{}'".format(id_votacao))
+    eleicao = cur.fetchone()
+    cur.execute("SELECT * FROM candidato WHERE id = '{}'".format(eleicao[3]))
+    candidato_um = cur.fetchone()
+    cur.execute("SELECT * FROM candidato WHERE id = '{}'".format(eleicao[4]))
+    candidato_dois = cur.fetchone()
+    if request.method == 'POST':
+        pass    
+    return render_template('votacao.html', candidato1 = candidato_um, candidato2 = candidato_dois)
